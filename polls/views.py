e719a2c1657bc03152460ed83829b2372b2e65fb
evflujan <evflujan@gmail.com>
2021-05-15 12:55:54 +0200

    Ajuste al Tutorial 3: Writing your first Django app

--- polls/views.py
+++ polls/views.py
@@ -9,19 +9,23 @@ from .models import Question
 
 
 def index(request):
+    template = 'polls/index.html'
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    return render(request, template, context)
 
 
 def detail(request, question_id):
+    template = 'polls/detail.html'
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    context = {'question': question}
+    return render(request, template, context)
 
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
