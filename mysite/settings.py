cb116c1989f915b7150b3d6305c852540347abc1
evflujan <evflujan@gmail.com>
2021-05-16 14:03:45 +0200

    Especificación correcta de style.css

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,8 +121,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = "~/django_project/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+'''
+STATICFILES_DIRS = [
+    # ...
+    ("style", "~/django_project/static"),
+]
+'''
