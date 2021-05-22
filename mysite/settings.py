04f448b600f5aa4f02c16267a8ae01cedb77dfc1
evflujan <evflujan@gmail.com>
2021-05-22 16:55:05 +0200

    Ultimas modificaciones aplicación polls, tutorial 4. Incorporación app gview procedente de dj4e.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [ "*" ]
 # Application definition
 
 INSTALLED_APPS = [
+    'gview.apps.GviewConfig',
     'books.apps.BooksConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
