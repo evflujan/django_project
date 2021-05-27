0c22ee6770de98b4ae59413c5637e575764dba54
evflujan <evflujan@gmail.com>
2021-05-27 18:47:51 +0200

    Ejercicio Dj4E sessions. Creación de app home

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,12 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+import os
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+from django.contrib.auth import views as auth_views
+from django.views.static import serve
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home/main.html')),
+#    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
     path('polls/', include('polls.urls')),
     path('gview/', include('gview.urls')),
+    path('home/', include('home.urls')),
 ]
