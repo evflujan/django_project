3252fc084e2cc6c494cf89b2080edd1c2331ed75
evflujan <evflujan@gmail.com>
2021-06-29 09:39:43 +0200

    Incorporar aplicaciones PICS y FORUMS

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [ "*" ]
 # Application definition
 
 INSTALLED_APPS = [
+    'pics.apps.PicsConfig',
     'ads.apps.AdsConfig',
     'cats.apps.CatsConfig',
     'autos.apps.AutosConfig',
